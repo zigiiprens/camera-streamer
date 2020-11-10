599bbf6d728e3c0c38b046161e8c70aa0e4e3a5c
zigiiprens <samosyzigii1@gmail.com>
2020-11-10 17:21:58 +0300

    teststream with jetson nano RSTP IP camera and GSTREAMER

--- src/teststream.py
+++ src/teststream.py
@@ -2,13 +2,18 @@
 import cv2 as cv
 import time
 
-__default_width__ = 640
-__default_height__ = 480
+__default_width__ = 1920
+__default_height__ = 1080
 
-api = "rtsp://10.42.0.204:5544/MainStream"
+api = "rtsp://admin:@10.42.0.204:5544/MainStream"
+gst = ('rtspsrc location={} latency={} ! application/x-rtp, payload=96, encoding-name=H264 !'
+		'rtpjitterbuffer mode=1 ! rtph264depay !'
+               	' h264parse ! nvv4l2decoder ! nvvidconv! video/x-raw, format=BGRx !'
+               	' videoconvert ! video/x-raw, format=BGR ! appsink ').format(api, 0)
 webcam = 0
 
-cap = cv.VideoCapture(webcam)
+cap = cv.VideoCapture(gst, cv.CAP_GSTREAMER)
+# cap = cv.VideoCapture(api)
 time.sleep(2.0)
 
 width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
